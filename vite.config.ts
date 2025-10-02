1a9212eb1d9d5eb4851098ee0c2470e85d1cd2cf
AmoghGK <amoghkotabagi007@gmail.com>
2025-10-03 01:09:58 +0530

    fix: 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: process.env.NODE_ENV === 'production' ? '/DSA-Problems-Tracker/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
