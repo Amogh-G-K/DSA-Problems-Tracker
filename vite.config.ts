4bb0847bd28351624fd5a6a5dd065c75242e6afa
AmoghGK <amoghkotabagi007@gmail.com>
2025-10-03 01:16:05 +0530

    fix: white screen

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: '/',
+  base: '/DSA-Problems-Tracker/',
   server: {
     host: "::",
     port: 8080,
